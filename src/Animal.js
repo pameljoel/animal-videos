45c1bde4bec6ae8af489c418d8e86d5250d20063
jbeltre <joel.beltre@lastminute.com>
2020-05-01 11:24:14 +0200

    when an animal is selected you can't click it again

--- src/Animal.js
+++ src/Animal.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 export function Animal (props) {
-  const {name, url, keywords, handleClick } = props;
-  return <div className="animal" onClick={() => handleClick(name, keywords) }>
+  const {name, url, keywords, handleClick, isSelected } = props;
+  const isSelectedClass = isSelected ? 'animal--selected' : '';
+  return <div className={`animal ${isSelectedClass}`} onClick={() => !isSelected && handleClick(name, keywords) }>
     <div className="image" >
     <img src={url} alt={name} />
     </div>
--- src/App.js
+++ src/App.js
@@ -9,55 +9,10 @@ import './Video.scss';
 import {Video} from "./Video";
 const KEY = 'AIzaSyCth3fzhizoG-9vOyfZ1xRnyLMHmQOvpmc';
 
-const stub = {
-  "kind": "youtube#searchListResponse",
-  "etag": "\"Dn5xIderbhAnUk5TAW0qkFFir0M/otdBITGO-15cj3VScUektCJFD-M\"",
-  "nextPageToken": "CAUQAA",
-  "regionCode": "IT",
-  "pageInfo": {
-    "totalResults": 1000000,
-    "resultsPerPage": 5
-  },
-  "items": [
-    {
-      "kind": "youtube#searchResult",
-      "etag": "\"Dn5xIderbhAnUk5TAW0qkFFir0M/n5zkS73ly2jkR5NPCbTZQbI5LXA\"",
-      "id": {
-        "kind": "youtube#video",
-        "videoId": "SB-qEYVdvXA"
-      },
-      "snippet": {
-        "publishedAt": "2018-03-19T02:00:25.000Z",
-        "channelId": "UCq5hgY37WAryZCwmehDyCaQ",
-        "title": "So many cute kittens videos compilation 2018",
-        "description": "Bravecto #anipetshop.com ✈✈✈Shop Pet Medicines : https://anipetshop.com ©BUY NOW : Bravecto, Nexgard, Heartgard, Revolution,..... ▻Worldwide FREE ...",
-        "thumbnails": {
-          "default": {
-            "url": "https://i.ytimg.com/vi/SB-qEYVdvXA/default.jpg",
-            "width": 120,
-            "height": 90
-          },
-          "medium": {
-            "url": "https://i.ytimg.com/vi/SB-qEYVdvXA/mqdefault.jpg",
-            "width": 320,
-            "height": 180
-          },
-          "high": {
-            "url": "https://i.ytimg.com/vi/SB-qEYVdvXA/hqdefault.jpg",
-            "width": 480,
-            "height": 360
-          }
-        },
-        "channelTitle": "Funny Videos",
-        "liveBroadcastContent": "none"
-      }
-    },
-  ]
-};
-
 function App() {
   const [animals, setAnimals] = useState([]);
-  const [videos, setVideos] = useState(stub.items);
+  const [selected, setSelected] = useState(null);
+  const [videos, setVideos] = useState([]);
 
   useEffect(() => {
     setAnimals(AnimalsJson);
@@ -71,7 +26,6 @@ function App() {
       })
       .catch(err => {
         console.log(err);
-        setVideos(stub.items);
       });
   };
 
@@ -81,8 +35,8 @@ function App() {
 
   const handleClick = (name, keywords) => {
     const keyword = _.sample(keywords);
+    setSelected(name);
     getVideoFromKeyword(keyword);
-
   };
 
   return (
@@ -94,7 +48,8 @@ function App() {
       {animals && <div className="animals">
         {animals.map(animal => {
           const {name, url, keywords} = animal;
-          return <Animal key={name} name={name} url={url} keywords={keywords} handleClick={handleClick}/>
+          const isSelected = animal.name === selected;
+          return <Animal key={name} name={name} url={url} keywords={keywords} handleClick={handleClick} isSelected={isSelected}/>
         })}
       </div>
       }
